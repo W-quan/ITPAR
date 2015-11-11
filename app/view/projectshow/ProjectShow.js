ebf786093ea25f46d3a45a003d7d02d8ac215ae9
W-quan <863102331@qq.com>
2015-11-11 18:52:57 +0800

    完成登陆,请求首页面功能

--- app/view/projectshow/ProjectShow.js
+++ app/view/projectshow/ProjectShow.js
@@ -4,11 +4,6 @@
 Ext.define('ITPAR.view.projectshow.ProjectShow', {
     extend: 'Ext.panel.Panel',
 
-    requires: [
-	    'ITPAR.view.projectshow.ProjectShowController',
-	    'ITPAR.view.projectshow.ProjectShowModel'
-    ],
-
     xtype: 'projectshow',
 
 	config:{
@@ -20,8 +15,9 @@ Ext.define('ITPAR.view.projectshow.ProjectShow', {
     },
     controller: 'projectshow',
 
-	margin: '10 20 10 20',
+	margin: '10 00 10 20',
 	autoScroll: true,
+	closable: true,
 
 	title: '项目展示',
 
@@ -29,10 +25,10 @@ Ext.define('ITPAR.view.projectshow.ProjectShow', {
 		'<h1  style="text-align:center">{title}</h1>',
 			'<p style="text-align:center;">{publisher} {pdate}</p>',
 		'<h2>项目简介</h2>' ,
-			'<p style="text-indent:2em">{abstractt}</p>' ,
+			'<p style="text-indent:2em; margin-right: 10px">{abstractt}</p>' ,
 		'<h2>项目详细内容</h2> ' ,
 			'<tpl for="details">',
-				'<p style="text-indent:2em">{detail}</p>' ,
+				'<p style="text-indent:2em; margin-right: 10px">{detail}</p>' ,
 					'<div style="margin: 0 auto; text-align: center;">' ,
 						'<img src="{image1}" alt="img1" width="150px" height="150px" style="margin: 5px"/>' ,
 						'<img src="{image2}" alt="img2" width="150px" height="150px" style="margin: 5px"/>' ,
