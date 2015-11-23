76625f3415e54cb91e5c5e88d90a31d4fe980891
W-quan <863102331@qq.com>
2015-11-23 17:51:19 +0800

    完成发布新文档功能

--- app/view/projectdiscussion/newdoc/NewDoc.js
+++ app/view/projectdiscussion/newdoc/NewDoc.js
@@ -2,61 +2,74 @@
  * Created by wzq on 15-11-2.
  */
 Ext.define('ITPAR.view.projectdiscussion.newdoc.NewDoc', {
-    extend: 'Ext.window.Window',
+	extend: 'Ext.window.Window',
 
-    requires: [
-	    'Ext.form.Panel',
-	    'Ext.form.field.File',
-	    'Ext.form.field.Text',
-	    'Ext.form.field.TextArea',
-	    'Ext.layout.container.VBox',
-	    'ITPAR.view.projectdiscussion.newdoc.NewDocController',
-	    'ITPAR.view.projectdiscussion.newdoc.NewDocModel'
-    ],
+	requires: [
+		'Ext.form.Panel',
+		'Ext.form.field.File',
+		'Ext.form.field.Text',
+		'Ext.form.field.TextArea',
+		'Ext.layout.container.VBox',
+		'ITPAR.view.projectdiscussion.newdoc.NewDocController',
+		'ITPAR.view.projectdiscussion.newdoc.NewDocModel'
+	],
 
-    xtype: 'newdoc',
+	xtype: 'newdoc',
 
-    viewModel: {
-        type: 'newdoc'
-    },
+	viewModel: {
+		type: 'newdoc'
+	},
 
-    controller: 'newdoc',
+	controller: 'newdoc',
 
 	width: 600,
 	height: 280,
 
-    items: [{
-	    html: '<h1  style="text-align:center">发布新文档</h1>'
-    },{
-	    xtype: 'form',
-	    margin: '10 10 10 10',
-	    layout: {
-		    type: 'vbox',
-		    align: 'stretch'
-	    },
+	modal: true,
 
-	    items: [{
-		    xtype: 'textfield',
-		    fieldLabel: '文档名称',
-		    name: 'docname'
-	    },{
-		    xtype: 'textarea',
-		    fieldLabel: '文档内容描述',
-		    name: 'docIntroduction'
-		    //height: 100
-	    },{
-		    xtype: 'filefield',
-		    fieldLabel: '文档文件',
-		    buttonText: '选择'
-	    }],
+	items: [{
+		html: '<h1  style="text-align:center">发布新文档</h1>'
+	}, {
+		xtype: 'form',
+		margin: '10 10 10 10',
+		layout: {
+			type: 'vbox',
+			align: 'stretch'
+		},
+		reference: 'newdocform',
 
-	    buttonAlign: 'center',
-	    buttons: [{
-		    text: '提交'
-	    },{
-		    text: '取消'
-	    }]
+		items: [{
+			xytpe: 'textfield',
+			name: 'discuss',
+			reference: 'discussId',
+			hidden: true
+		},{
+			xtype: 'textfield',
+			fieldLabel: '文档名称',
+			name: 'title'
+		}, {
+			xtype: 'textarea',
+			fieldLabel: '文档内容描述',
+			name: 'abstractt'
+		}, {
+			xtype: 'fileuploadfield',
+			name: 'doc',
+			fieldLabel: '文档文件',
+			buttonText: '选择'
+		}],
 
-    }
-    ]
+		buttonAlign: 'center',
+		buttons: [{
+			text: '提交',
+			listeners: {
+				click: 'newDocSubmit'
+			}
+		}, {
+			text: '取消',
+			listeners: {
+				click: 'closeWindows'
+			}
+		}]
+
+	}]
 });
